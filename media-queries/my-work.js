689ef9ab882dc24ff7a2c6eb8ccb9d67c8935404
BoltenCoder <moltencreeper25@gmail.com>
2025-04-14 18:38:19 -0500

    Added full functionality to map navigation on my work page.

---
+++ media-queries/my-work.js
@@ -0,0 +1,20 @@
+document.querySelector('.NM0').addEventListener('click', () => {
+    // Scroll to the top of the page
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// Function to handle the click event on the "Read More" button
+document.querySelector('.NM1').addEventListener('click', () => {
+    // Scroll to the summary section
+    document.querySelector('.living-planet').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.NM2').addEventListener('click', () => {
+    // Scroll to the summary section
+    document.querySelector('.max1m').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.NM3').addEventListener('click', () => {
+    // Scroll to the summary section
+    document.querySelector('.charging-box').scrollIntoView({ behavior: 'smooth' });
+});
